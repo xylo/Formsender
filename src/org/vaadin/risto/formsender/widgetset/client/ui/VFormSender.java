421c871b084b58e9b7a4a359ef7be0aebfbd520a
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-10-17 23:07:44 +0300

    Moving to git

--- src/org/vaadin/risto/formsender/widgetset/client/ui/VFormSender.java
+++ src/org/vaadin/risto/formsender/widgetset/client/ui/VFormSender.java
@@ -24,13 +24,7 @@ public class VFormSender extends Widget implements Paintable {
      * then handle any initialization relevant to Vaadin.
      */
     public VFormSender() {
-        // TODO Example code is extending GWT Widget so it must set a root
-        // element.
-        // Change to proper element or remove if extending another widget
         setElement(Document.get().createDivElement());
-
-        // This method call of the Paintable interface sets the component
-        // style name in DOM tree
         setStyleName(CLASSNAME);
     }
 
@@ -38,20 +32,12 @@ public class VFormSender extends Widget implements Paintable {
      * Called whenever an update is received from the server
      */
     public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
-        // This call should be made first.
-        // It handles sizes, captions, tooltips, etc. automatically.
         if (client.updateComponent(this, uidl, true)) {
-            // If client.updateComponent returns true there has been no changes
-            // and we
-            // do not need to update anything.
             return;
         }
 
-        // Save reference to server connection object to be able to send
-        // user interaction later
         this.client = client;
 
-        // Save the client side identifier (paintable id) for the widget
         paintableId = uidl.getId();
 
         if (uidl.hasAttribute("submit") && uidl.getBooleanAttribute("submit")) {
